e19d9796730815c2468231b3382d32b1f9568a1a
Donald Atkinson <fuzzicallogic@outlook.com>
2016-01-01 22:13:37 -0700

    Initial Element Setup

---
+++ register.js
@@ -0,0 +1,27 @@
+(function registerElement() {
+	Polymer({
+		is: 'native-window',
+
+		created: onElementCreated,
+		attached: onElementAttached,
+		ready: onElementReady,
+		detached: onElementDetached,
+	});
+
+	function onElementCreated() {
+
+	}
+
+	function onElementAttached() {
+
+	}
+
+	function onElementReady() {
+
+	}
+
+	function onElementDetached() {
+
+	}
+
+}) ();
